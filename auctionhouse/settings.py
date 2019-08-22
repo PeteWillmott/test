688e139288f3a66136275ccc6e80e3ccba8114fe
PeteWillmott <bastior@yahoo.co.uk>
2019-08-22 11:10:36 +0200

    Added sample data to catalogue db, changed settings to disply images from db.

--- auctionhouse/settings.py
+++ auctionhouse/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bsyna5z8nhr$81kq1(01y#yn7#d@7h^ng9khkl88%tpmlq$_il'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.media',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
@@ -123,3 +124,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
