801614ad10651420fb9d7a790639f8498901ec02
PeteWillmott <bastior@yahoo.co.uk>
2019-09-04 07:54:42 +0200

    Added News views and review template.

--- news/views.py
+++ news/views.py
@@ -1,2 +1,12 @@
 from django.shortcuts import render
+from .models import News, Review
+
+def news(request):
+    article = News.objects.last()
+    return render(request, 'lead_news_item.html', {"article": article})
+
+
+def review(request):
+    review = Review.objects.all()
+    return render(request, 'reviews.html', {"review": review})
 
