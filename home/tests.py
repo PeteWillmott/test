a99f1079244bf7b921808a70b1f78f1516417b41
PeteWillmott <bastior@yahoo.co.uk>
2019-09-30 18:37:59 +0200

    Home passes testing.

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestDisplayViews(TestCase):
+
+    def test_display_all_view(self):
+        page = self.client.get("/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "index.html")
+
+    def test_display_all_view(self):
+        page = self.client.get("/about")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "about.html")
+
+    def test_display_all_view(self):
+        page = self.client.get("/contact")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "contact.html")
+
+    def test_display_all_view(self):
+        page = self.client.get("/FAQ")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "faq.html")
+
+    def test_display_all_view(self):
+        page = self.client.get("/how-to")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "how-to.html")
+
+    def test_display_all_view(self):
+        page = self.client.get("/shipping")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "shipping.html")
