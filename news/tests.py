1b1ac3a1abca97c3e524b5f2c447fc48e1c68451
PeteWillmott <bastior@yahoo.co.uk>
2019-09-30 19:17:07 +0200

    News passes testing.

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestDisplayViews(TestCase):
+
+    def test_news_view(self):
+        page = self.client.get("/news")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "news.html")
+
+    def test_review_view(self):
+        page = self.client.get("/reviews")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "reviews.html")
