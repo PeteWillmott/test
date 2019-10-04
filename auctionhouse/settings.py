802081a7e630a3c9317e7ecdc1e714ad64859520
PeteWillmott <bastior@yahoo.co.uk>
2019-10-04 11:01:59 +0200

    Re-re-re-re-deploying to heroku.

--- auctionhouse/settings.py
+++ auctionhouse/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'eezzy$461d(ogu=!n#)+yztu+c@qoyex-9gvw^$#&)1_%(*=h='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ms4-auctionsite.herokuapp.com/']
 
 
 # Application definition
