8c808b3de75aeb35db17951dc08ad7072981b5c1
PeteWillmott <bastior@yahoo.co.uk>
2019-10-05 11:17:27 +0200

    Reset debug to true.

--- auctionhouse/settings.py
+++ auctionhouse/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ms4-auctionsite.herokuapp.com/']
 
