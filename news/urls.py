23b29e27efb3fa3de1bac697ab1f8265783e588c
PeteWillmott <bastior@yahoo.co.uk>
2019-10-05 12:47:25 +0200

    Corrected links on base.html for blog namespace.

--- news/urls.py
+++ news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import news, reviews
 from .models import News, Review
 
-appname = 'news'
+app_name = 'blog'
 urlpatterns = [
     path('news', news, name="news"),
     path('reviews', reviews, name="reviews"),
