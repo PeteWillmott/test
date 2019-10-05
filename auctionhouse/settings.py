34ecee62e8af57d17a71ba06c83d29d06bb5c69c
PeteWillmott <bastior@yahoo.co.uk>
2019-10-05 11:11:09 +0200

    Added django-heroku.

--- auctionhouse/settings.py
+++ auctionhouse/settings.py
@@ -23,13 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'eezzy$461d(ogu=!n#)+yztu+c@qoyex-9gvw^$#&)1_%(*=h='
+# SECRET_KEY = 'eezzy$461d(ogu=!n#)+yztu+c@qoyex-9gvw^$#&)1_%(*=h='
+SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ms4-auctionsite.herokuapp.com/']
-
 
 # Application definition
 
